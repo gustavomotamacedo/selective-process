dd635dd145cee5139717da133262403fc0823298
gustavomotamacedo <mota.macedo05@gmail.com>
2023-10-06 15:55:32 -0300

    função analisarCandidato()

--- src/opensource/gustavomotamacedo/candidaturas/ProcessoSeletivo.java
+++ src/opensource/gustavomotamacedo/candidaturas/ProcessoSeletivo.java
@@ -1,5 +1,19 @@
 package opensource.gustavomotamacedo.candidaturas;
 
 public class ProcessoSeletivo {
-
+	public static void analisarCandidato(double salarioPretendido) {
+		double salarioBase = 2000.0;
+		if (salarioBase > salarioPretendido)
+			System.out.println("Ligar para o candidato.");
+		else if(salarioBase == salarioPretendido)
+			System.out.println("Ligar para o candidato, com contraproposta.");
+		else
+			System.out.println("Aguardando demais candidatos ");
+	}
+	
+	public static void main(String[] args) {
+		analisarCandidato(2000.0);
+		analisarCandidato(1900.0);
+		analisarCandidato(1200.0);
+	}
 }
